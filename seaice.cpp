40ed338e2fef85659abeb00699e32ba1646132bf
Ariz Chang <abchang@asu.edu>
2019-11-14 16:42:05 -0700

    added structs for node and graph

--- seaice.cpp
+++ seaice.cpp
@@ -3,15 +3,34 @@
 
 using namespace std;
 
+//defn for nodes for adjacency list
+struct node
+{
+  int vertex;
+  int* data; //array that will hold data from all the weeks and years
+  struct node* next;
+};
+
+//defn for graph data structure
+struct graph
+{
+  int numVertices;
+  struct node*** adjacencyList; //2D array of pointers to nodes
+};
+
 int main(int argc, char** argv)
 {
   ifstream inputFile("1990/Beaufort_Sea_diffw01y1990+landmask", ios::in | ios::binary);
   float dataIn = 0;
+  int count = 0;
   inputFile.read((char*)&dataIn, 4);
   while(!inputFile.eof())
     {
-      cout << dataIn << endl;
+      cout << dataIn << " ";
       inputFile.read((char*)&dataIn, 4);
+      count++;
+      if(count % 63 == 0)
+	cout << endl;
     }
   inputFile.close();
   return 0;
