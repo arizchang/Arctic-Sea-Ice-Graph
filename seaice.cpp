65f03a444503594a2cf67d36ea77ee4a1bd8e429
Ariz Chang <abchang@asu.edu>
2019-11-16 18:38:06 -0700

    added fill means array function (not working yet)

--- seaice.cpp
+++ seaice.cpp
@@ -4,9 +4,6 @@
 
 using namespace std;
 
-//functions used
-
-
 //defn for nodes for adjacency list
 struct node
 {
@@ -22,12 +19,16 @@ struct graph
   struct node** adjacencyList; //array of pointers to nodes for adjacency list
 };
 
+//functions used
+void fillMeans(graph*, float*&);
+
 int main(int argc, char** argv)
 {
   //make new graph
   graph* iceGraph = new graph;
   iceGraph->numVertices = 3969;
   iceGraph->adjacencyList = new node*[iceGraph->numVertices];
+  float* means = new float[3969];
 
   //declare arrays of data for all vertices
   for(int i = 0; i < iceGraph->numVertices; i++)
@@ -46,7 +47,7 @@ int main(int argc, char** argv)
   int time = 0;
 
   //iterating through all year folders
-    for(int i = 0; i < 16; i++)
+  for(int i = 0; i < 16; i++)
     {
       year = 1990 + i;
       //iterating through all weeks
@@ -74,11 +75,12 @@ int main(int argc, char** argv)
 	  inputFile.close();
 	} 
     }
-  
+
+  //fills array of means
+  fillMeans(iceGraph, means);
+
   for(int i = 0; i < 832; i++)
-    {
-      cout << iceGraph->adjacencyList[16]->data[i] << endl;
-    }
+    cout << means[i] << endl;
 
   /*  
   ifstream inputFile("2005/Beaufort_Sea_diffw52y2005+landmask", ios::in | ios::binary);
@@ -96,4 +98,33 @@ int main(int argc, char** argv)
   inputFile.close();
   */
   return 0;
+}
+
+//fills array of means
+void fillMeans(graph* iceGraph, float*& means)
+{
+  float sum = 0;
+  float mean = 0;
+  float numIce = 0; //vertices that are not land
+
+  //determines how many vertices are actually ice
+  for(int i = 0; i < 3969; i++)
+    {
+      if(iceGraph->adjacencyList[i]->data[0] != 168)
+	numIce++;
+    }
+
+  //fills array of means
+  for(int i = 0; i < 3969; i++)
+    {
+      if(iceGraph->adjacencyList[i]->data[0] != 168)
+	{
+	  for(int j = 0; j < 832; j++)
+	    {
+	      sum += iceGraph->adjacencyList[i]->data[j];
+	    }
+	  mean = sum/832;
+	  means[i] = mean;
+	}
+    } 
 }
